46e69d2cf75ae1c8808c8898493580146042e871
akp4657 <31463463+akp4657@users.noreply.github.com>
2019-09-22 14:26:26 -0400

    Shapes and updates, need to go to office hours

--- src/shapes.js
+++ src/shapes.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Background circles
 function drawRectangles(ctx)
 {
     ctx.save();
@@ -13,7 +14,40 @@ function drawRectangles(ctx)
     ctx.restore();
 }
 
-function drawCircles(ctx)
+function drawCircles(ctx, radius)
+{
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(255,111,111,.34)'; 
+    ctx.arc(250,100,radius,0,2*Math.PI, false);
+    ctx.fill();
+    ctx.closePath();
+    
+    ctx.beginPath();
+    ctx.fillStyle = 'red'; 
+    ctx.arc(750,100,radius,0,2*Math.PI, false);
+    ctx.fill();
+    ctx.closePath();
+                
+}
+
+function drawTriangle(ctx, audio, topSpacing, num)
+{
+    ctx.beginPath();
+    //ctx.moveTo(500, topSpacing +256-audio[num]);
+    ctx.fillStyle = 'green';
+    ctx.arc(500,300-audio[num]/2,3,0,2*Math.PI, false);
+    //ctx.lineTo(460, 256 - audio[num]+100);
+    //ctx.lineTo(460, 256 - audio[num]+100);
+    ctx.fill();
+    ctx.closePath();
+    ctx.strokeStyle = 'green';
+    ctx.lineWidth = '1';
+    ctx.stroke();
+    //ctx.clearRect(0,0,1000,600);
+
+}
+
+function drawPoppy(ctx)
 {
     
 }
