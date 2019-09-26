496670c52d6494ef9bbdcde0f49c54a0d5d50197
Anthony <akp4657@rit.edu>
2019-09-25 23:59:39 -0400

    All shapes relative to window

--- src/shapes.js
+++ src/shapes.js
@@ -14,31 +14,34 @@ function drawRectangles(ctx, canvas)
     ctx.restore();
 }
 
-function drawCircles(ctx, radius)
+function drawCircles(ctx, canvas, audio, num)
 {
+    // Get the wave bin (audiodata i )
+    // Have the radius bounce off a percentage of that 
+    // Make the circle solid 
     ctx.save();
     ctx.beginPath();
     ctx.fillStyle = 'rgba(255,111,111,.34)'; 
-    ctx.arc(250,100,radius,0,2*Math.PI, false);
+    ctx.arc((canvas.width*(2/5)),(canvas.height*(2/7)),audio[num]/3,0,2*Math.PI, false);
     ctx.fill();
     ctx.closePath();
     
     ctx.beginPath();
     ctx.fillStyle = 'rgba(255,111,111,.34)'; 
-    ctx.arc(750,100,radius,0,2*Math.PI, false);
+    ctx.arc((canvas.width*(3/5)),(canvas.height*(2/7)),audio[num]/3,0,2*Math.PI, false);
     ctx.fill();
     ctx.closePath();
     ctx.restore();
                 
 }
 
-function drawTriangle(ctx, audio, topSpacing, num)
+function drawTriangle(ctx, audio, canvas, num)
 { 
     ctx.beginPath();
     ctx.fillStyle = 'rgba(255,111,111,.34)';
-    ctx.moveTo(500,300-audio[num]/1.5);
-    ctx.lineTo(500-audio[num]/1.5,300+(audio[num]/2.5)); 
-    ctx.lineTo(500+audio[num]/1.5,300+(audio[num]/2.5));
+    ctx.moveTo(canvas.width/2,canvas.height/2-audio[num]/1.5);
+    ctx.lineTo(canvas.width/2-audio[num]/1.5,canvas.height/2+(audio[num]/2.5)); 
+    ctx.lineTo(canvas.width/2+audio[num]/1.5,canvas.height/2+(audio[num]/2.5));
     ctx.fill();
     ctx.closePath();
     ctx.stroke();
@@ -52,13 +55,13 @@ function drawCurves(ctx, audio, canvas, num)
     ctx.strokeStyle = "black";
     ctx.beginPath();
     ctx.moveTo(canvas.width/2, 0);
-    ctx.quadraticCurveTo(-audio[num], canvas.height/2, canvas.width/2, canvas.height);
+    ctx.quadraticCurveTo(-audio[num]*2, canvas.height/2, canvas.width/2, canvas.height);
     ctx.stroke();
     
     ctx.strokeStyle = "white";
     ctx.beginPath();
-    ctx.moveTo(500, 600);
-    ctx.quadraticCurveTo(1000+audio[num], 300, 500, 0);
+    ctx.moveTo(canvas.width/2, canvas.height);
+    ctx.quadraticCurveTo((canvas.width/(17/18))+(audio[num]*2), canvas.height/2, canvas.width/2, 0);
     ctx.stroke();
     ctx.restore();
     
