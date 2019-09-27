267d53e00a39f5cb272f05172c756912d63364b0
Anthony <akp4657@rit.edu>
2019-09-26 22:26:52 -0400

    Gradients work!
    Not the final colors to be used

--- src/shapes.js
+++ src/shapes.js
@@ -19,15 +19,19 @@ function drawCircles(ctx, canvas, audio, num)
     // Get the wave bin (audiodata i )
     // Have the radius bounce off a percentage of that 
     // Make the circle solid 
+    /****The gradient colors are temporary****/
     var grad = ctx.createRadialGradient((canvas.width*(2/5)),(canvas.height*(2/7)),audio[10]/12, (canvas.width*(2/5)),(canvas.height*(2/7)),audio[14]/2);
-    //var grad = ctx.createRadialGradient()
     grad.addColorStop(0,'red');
-    grad.addColorStop(1,'orange');
-//    var grad1 = ctx.createRadialGradient(canvas.width*(3/5)),(canvas.height*(2/7)),audio[10]/12, (canvas.width*(3/5)),(canvas.height*(2/7)),audio[14]/2);
-//    grad1.addColorStop(0,'red');
-//    grad1.addColorStop(1,'orange');
+    grad.addColorStop(1/3,'orange');
+    grad.addColorStop(2/3,'yellow');
+    grad.addColorStop(1,'green');
     
-    ctx.save();
+    var grad2 = ctx.createRadialGradient((canvas.width*(3/5)),(canvas.height*(2/7)),audio[10]/12, (canvas.width*(3/5)),(canvas.height*(2/7)),audio[14]/2);
+    grad2.addColorStop(0,'red');
+    grad2.addColorStop(1/3,'orange');
+    grad2.addColorStop(2/3,'yellow');
+    grad2.addColorStop(1,'green');
+    
     ctx.beginPath();
     ctx.fillStyle = grad; 
     ctx.arc((canvas.width*(2/5)),(canvas.height*(2/7)),audio[14]/2.5,0,2*Math.PI, false);
@@ -35,11 +39,10 @@ function drawCircles(ctx, canvas, audio, num)
     ctx.closePath();
     
     ctx.beginPath();
-    ctx.fillStyle = grad; 
-    ctx.arc((canvas.width*(3/5)),(canvas.height*(2/7)),audio[14]/2,0,2*Math.PI, false);
+    ctx.fillStyle = grad2; 
+    ctx.arc((canvas.width*(3/5)),(canvas.height*(2/7)),audio[14]/2.5,0,2*Math.PI, false);
     ctx.fill();
     ctx.closePath();
-    ctx.restore();
                 
 }
 
